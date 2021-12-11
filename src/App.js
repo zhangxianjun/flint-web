93e9f5c23a58b6eac0170ac85307239e3e426960
zhangxianjun <1581758003@qq.com>
2021-12-11 13:50:01 +0800

    feat - 引入别人布局组件

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import './App.css';
-import Login from './controller/system/login/Login';
-import Try from './controller/try/Try';
-import TryClass from './controller/try/TryClass';
+import './view/BasicLayout'
+import BasicLayout from './view/BasicLayout';
 
 
 function App() {
   return (
-    <div className="App">
-        <Try name="React"></Try>      
-        <TryClass name="React"></TryClass>   
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-        <Login name="Mark" />
+    <div>
+        <BasicLayout>
+          
+        </BasicLayout>
     </div>
   );
 }
